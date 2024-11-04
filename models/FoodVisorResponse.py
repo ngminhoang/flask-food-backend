8ee2d5e3f7f29296c6a573bd7c084abdb6ec1490
ngminhoang <hoang8ctt@gmail.com>
2024-11-04 08:49:14 +0700

    update

--- models/FoodVisorResponse.py
+++ models/FoodVisorResponse.py
@@ -1,80 +1,132 @@
 from typing import List
-from __future__ import annotations  # Enables forward references
 
-class FoodAnalysisResponse:
-    def __init__(self, analysis_id, items):
+
+class AnalysisResponse:
+    def __init__(self, analysis_id: str, scopes: List[str], items: List['Item']):
         self.analysis_id = analysis_id
+        self.scopes = scopes
         self.items = items
 
     def to_dict(self):
         return {
-            "analysis_id": self.analysis_id,
-            "items": [item.to_dict() for item in self.items]
+            'analysis_id': self.analysis_id,
+            'scopes': self.scopes,
+            'items': [item.to_dict() for item in self.items]
         }
 
-class AnalysisItem:
-    def __init__(self, food):
+class Item:
+    def __init__(self, position: 'Position', food: List['Food']):
+        self.position = position
         self.food = food
 
     def to_dict(self):
         return {
-            "food": [f.to_dict() for f in self.food]
+            'position': self.position.to_dict(),
+            'food': [food.to_dict() for food in self.food]
         }
 
-class FoodItem:
-    def __init__(self, confidence, food_info, ingredients):
-        self.confidence = confidence
-        self.food_info = food_info
-        self.ingredients = ingredients
+class Position:
+    def __init__(self, x: float, y: float, width: float, height: float):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
     def to_dict(self):
         return {
-            "confidence": self.confidence,
-            "food_info": self.food_info.to_dict() if self.food_info else None,
-            "ingredients": [ingredient.to_dict() for ingredient in self.ingredients]
+            'x': self.x,
+            'y': self.y,
+            'width': self.width,
+            'height': self.height
+        }
+
+class Food:
+    def __init__(self, confidence: float, quantity: float, ingredients: List[str], food_info: 'FoodInfo'):
+        self.confidence = confidence
+        self.quantity = quantity
+        self.ingredients = ingredients
+        self.food_info = food_info
+
+    def to_dict(self):
+        return {
+            'confidence': self.confidence,
+            'quantity': self.quantity,
+            'ingredients': self.ingredients,
+            'food_info': self.food_info.to_dict()
         }
 
 class FoodInfo:
-    def __init__(self, display_name, food_id, fv_grade, g_per_serving, nutrition):
-        self.display_name = display_name
+    def __init__(self, food_id: str, fv_grade: str, g_per_serving: float, display_name: str, nutrition: 'Nutrition'):
         self.food_id = food_id
         self.fv_grade = fv_grade
         self.g_per_serving = g_per_serving
+        self.display_name = display_name
         self.nutrition = nutrition
 
     def to_dict(self):
         return {
-            "display_name": self.display_name,
-            "food_id": self.food_id,
-            "fv_grade": self.fv_grade,
-            "g_per_serving": self.g_per_serving,
-            "nutrition": self.nutrition.to_dict() if self.nutrition else None
+            'food_id': self.food_id,
+            'fv_grade': self.fv_grade,
+            'g_per_serving': self.g_per_serving,
+            'display_name': self.display_name,
+            'nutrition': self.nutrition.to_dict()
         }
 
 class Nutrition:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def to_dict(self):
-        return self.__dict__
-
-class Ingredient:
-    def __init__(
-            self,
-            confidence: float,
-            food_info: FoodInfo,
-            ingredients: List[Ingredient],
-            quantity: float
-    ):
-        self.confidence = confidence
-        self.food_info = food_info
-        self.ingredients = ingredients
-        self.quantity = quantity
+    def __init__(self, alcohol_100g: float, calcium_100g: float, calories_100g: float, carbs_100g: float,
+                 cholesterol_100g: float, fat_100g: float, fibers_100g: float, proteins_100g: float,
+                 sugars_100g: float, vitamin_c_100g: float):
+        self.alcohol_100g = alcohol_100g
+        self.calcium_100g = calcium_100g
+        self.calories_100g = calories_100g
+        self.carbs_100g = carbs_100g
+        self.cholesterol_100g = cholesterol_100g
+        self.fat_100g = fat_100g
+        self.fibers_100g = fibers_100g
+        self.proteins_100g = proteins_100g
+        self.sugars_100g = sugars_100g
+        self.vitamin_c_100g = vitamin_c_100g
 
     def to_dict(self):
         return {
-            "confidence": self.confidence,
-            "food_info": self.food_info.to_dict() if self.food_info else None,
-            "ingredients": [ingredient.to_dict() for ingredient in self.ingredients],
-            "quantity": self.quantity
+            'alcohol_100g': self.alcohol_100g,
+            'calcium_100g': self.calcium_100g,
+            'calories_100g': self.calories_100g,
+            'carbs_100g': self.carbs_100g,
+            'cholesterol_100g': self.cholesterol_100g,
+            'fat_100g': self.fat_100g,
+            'fibers_100g': self.fibers_100g,
+            'proteins_100g': self.proteins_100g,
+            'sugars_100g': self.sugars_100g,
+            'vitamin_c_100g': self.vitamin_c_100g
         }
+
+from typing import Optional
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Ingradient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    nuGrams = db.Column(db.Float, nullable=True)
+    nuCalories = db.Column(db.Float, nullable=True)
+    nuProteins = db.Column(db.Float, nullable=True)
+    nuCarbs = db.Column(db.Float, nullable=True)
+    nuFibers = db.Column(db.Float, nullable=True)
+    nuFats = db.Column(db.Float, nullable=True)
+    nuSatFats = db.Column(db.Float, nullable=True)
+    nuPrice = db.Column(db.Float, nullable=True)
+    search_status = db.Column(db.Integer, default=0)  # Default to 0 (PENDING)
+
+    def __init__(self, name, nuGrams=None, nuCalories=None, nuProteins=None, nuCarbs=None, nuFibers=None,
+                 nuFats=None, nuSatFats=None, nuPrice=None):
+        self.name = name
+        self.nuGrams = nuGrams
+        self.nuCalories = nuCalories
+        self.nuProteins = nuProteins
+        self.nuCarbs = nuCarbs
+        self.nuFibers = nuFibers
+        self.nuFats = nuFats
+        self.nuSatFats = nuSatFats
+        self.nuPrice = nuPrice
--- services/DetectImageService.py
+++ services/DetectImageService.py
@@ -1,6 +1,73 @@
+from typing import List
+
 import requests
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from models.FoodVisorResponse import AnalysisResponse, Position, Nutrition, FoodInfo, Food, Item
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5437/food-db'
+db = SQLAlchemy(app)
+
+def parse_analysis_response(data: dict) -> AnalysisResponse:
+    analysis_id = data.get('analysis_id', '')
+    scopes = data.get('scopes', [])
+    items = []
+
+    for item_data in data.get('items', []):
+        position_data = item_data.get('position', {})
+        position = Position(
+            x=position_data.get('x', 0),
+            y=position_data.get('y', 0),
+            width=position_data.get('width', 0),
+            height=position_data.get('height', 0)
+        )
+
+        food_list = []
+        for food_data in item_data.get('food', []):
+            nutrition_data = food_data.get('food_info', {}).get('nutrition', {})
+            nutrition = Nutrition(
+                alcohol_100g=nutrition_data.get('alcohol_100g', 0),
+                calcium_100g=nutrition_data.get('calcium_100g', 0),
+                calories_100g=nutrition_data.get('calories_100g', 0),
+                carbs_100g=nutrition_data.get('carbs_100g', 0),
+                cholesterol_100g=nutrition_data.get('cholesterol_100g', 0),
+                fat_100g=nutrition_data.get('fat_100g', 0),
+                fibers_100g=nutrition_data.get('fibers_100g', 0),
+                proteins_100g=nutrition_data.get('proteins_100g', 0),
+                sugars_100g=nutrition_data.get('sugars_100g', 0),
+                vitamin_c_100g=nutrition_data.get('vitamin_c_100g', 0)
+            )
+
+            # <kiểm tra xem trong db ,trong bảng ingredient đã tồm tại hay chưa (không cần query vì trường name đã để là unique), nếu chưa tồn tại thì add vào db dựa theo class infradient (hãy hỗ trợ tôi mapping các trường dữ liệu phù hợp vào class )>
 
 
+            food_info_data = food_data.get('food_info', {})
+            food_info = FoodInfo(
+                food_id=food_info_data.get('food_id', ''),
+                fv_grade=food_info_data.get('fv_grade', ''),
+                g_per_serving=food_info_data.get('g_per_serving', 0),
+                display_name=food_info_data.get('display_name', ''),
+                nutrition=nutrition
+            )
+
+
+
+            food = Food(
+                confidence=food_data.get('confidence', 0),
+                quantity=food_data.get('quantity', 0),
+                ingredients=food_data.get('ingredients', []),
+                food_info=food_info
+            )
+            food_list.append(food)
+
+        item = Item(position=position, food=food_list)
+        items.append(item)
+
+    return AnalysisResponse(analysis_id=analysis_id, scopes=scopes, items=items)
+
+# Sử dụng hàm trong analyze_image
 def analyze_image(api_key, image_path):
     url = "https://vision.foodvisor.io/api/1.0/en/analysis/"
     headers = {"Authorization": f"Api-Key {api_key}"}
@@ -9,5 +76,82 @@ def analyze_image(api_key, image_path):
         response = requests.post(url, headers=headers, files={"image": image})
         response.raise_for_status()
 
+    # Chuyển đổi dữ liệu JSON thành đối tượng AnalysisResponse
+    analysis_response = parse_analysis_response(response.json())
+    return analysis_response.to_dict()
 
-    return response.json()
+
+#
+# def parse_analysis_response(data: dict) -> AnalysisResponse:
+#     analysis_id = data.get('analysis_id', '')
+#     scopes = data.get('scopes', [])
+#     items = []
+#
+#     for item_data in data.get('items', []):
+#         position_data = item_data.get('position', {})
+#         position = Position(
+#             x=position_data.get('x', 0),
+#             y=position_data.get('y', 0),
+#             width=position_data.get('width', 0),
+#             height=position_data.get('height', 0)
+#         )
+#
+#         food_list = []
+#         for food_data in item_data.get('food', []):
+#             nutrition_data = food_data.get('food_info', {}).get('nutrition', {})
+#             nutrition = Nutrition(
+#                 alcohol_100g=nutrition_data.get('alcohol_100g', 0),
+#                 calcium_100g=nutrition_data.get('calcium_100g', 0),
+#                 calories_100g=nutrition_data.get('calories_100g', 0),
+#                 carbs_100g=nutrition_data.get('carbs_100g', 0),
+#                 cholesterol_100g=nutrition_data.get('cholesterol_100g', 0),
+#                 fat_100g=nutrition_data.get('fat_100g', 0),
+#                 fibers_100g=nutrition_data.get('fibers_100g', 0),
+#                 proteins_100g=nutrition_data.get('proteins_100g', 0),
+#                 sugars_100g=nutrition_data.get('sugars_100g', 0),
+#                 vitamin_c_100g=nutrition_data.get('vitamin_c_100g', 0)
+#             )
+#
+#             # Kiểm tra và thêm vào DB
+#             ingredient_name = food_data.get('ingredients', [])[0] if food_data.get('ingredients') else "Unknown"
+#             existing_ingredient = Ingradient.query.filter_by(name=ingredient_name).first()
+#
+#             if not existing_ingredient:
+#                 # Nếu không tồn tại, tạo mới
+#                 new_ingredient = Ingradient(
+#                     name=ingredient_name,
+#                     nuGrams=nutrition.nuGrams,  # Giả định bạn có trường này trong Nutrition
+#                     nuCalories=nutrition.calories_100g,
+#                     nuProteins=nutrition.proteins_100g,
+#                     nuCarbs=nutrition.carbs_100g,
+#                     nuFibers=nutrition.fibers_100g,
+#                     nuFats=nutrition.fat_100g,
+#                     nuSatFats=nutrition.satFats,  # Giả định bạn có trường này trong Nutrition
+#                     nuPrice=None  # Thay thế với giá nếu có
+#                 )
+#                 db.session.add(new_ingredient)
+#
+#             food_info_data = food_data.get('food_info', {})
+#             food_info = FoodInfo(
+#                 food_id=food_info_data.get('food_id', ''),
+#                 fv_grade=food_info_data.get('fv_grade', ''),
+#                 g_per_serving=food_info_data.get('g_per_serving', 0),
+#                 display_name=food_info_data.get('display_name', ''),
+#                 nutrition=nutrition
+#             )
+#
+#             food = Food(
+#                 confidence=food_data.get('confidence', 0),
+#                 quantity=food_data.get('quantity', 0),
+#                 ingredients=food_data.get('ingredients', []),
+#                 food_info=food_info
+#             )
+#             food_list.append(food)
+#
+#         item = Item(position=position, food=food_list)
+#         items.append(item)
+#
+#     # Lưu tất cả các thay đổi vào DB
+#     db.session.commit()
+#
+#     return AnalysisResponse(analysis_id=analysis_id, scopes=scopes, items=items)
